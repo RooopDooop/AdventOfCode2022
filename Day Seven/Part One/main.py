17e8ef9ce7b21f67de8b567bad37c68e2adef93a
WatchMeJump <jacobmorin61@yahoo.ca>
2022-12-08 18:54:28 -0500

    Test with recursion

--- Day Seven/Part One/main.py
+++ Day Seven/Part One/main.py
@@ -1,8 +1,33 @@
+class AdventDir:
+    def __init__(self, name, numericValue, children):
+        self.name = name
+        self.numericValue = numericValue
+        self.children = children
+    #def findChildren(self, strTarget):
+        #for objChild in self.children:
+            #if objChild.name == strTarget:
+                #print("Found Child: " + objChild.name)
+                #break
+            #else:
+                #objChild.findChildren(strTarget)
+    def findParentAddChild(self, strParent, objChild):
+        print(self.name + " - " + strParent)
+        if self.name == strParent:
+            self.children.append(objChild)
+        else:
+            for objChild in self.children:
+                objChild.findParentAddChild(strParent, objChild)
+
+    #def addChild(self, objChild):
+        #self.children.append(objChild)
+
 def main():
     currentDirectory = []
 
-    allDirectories = dict({})
-    documentReverseIndex = dict({})
+    MainDir = AdventDir
+
+    #allDirectories = dict({})
+    #documentReverseIndex = dict({})
 
     with open("../inputData.txt", "r") as objFile:
         while True:
@@ -16,35 +41,47 @@ def main():
                 strCommand = strLine.split(" ")[1]
 
                 if strCommand == "cd":
-                    if currentDirectory == "":
+                    if strLine == "$ cd /":
                         currentDirectory.append(strLine.split(" ")[2])
-                        allDirectories[' '.join(currentDirectory)] = [dict()]
+                        MainDir = AdventDir("/", 0, [])
+
+                        #allDirectories[' '.join(currentDirectory)] = [dict()]
                     elif strLine.split(" ")[2] != "..":
                         currentDirectory.append(strLine.split(" ")[2])
 
-                        allDirectories[strLine.split(" ")[2]] = ' '.join(currentDirectory)
+                        #print(str(currentDirectory[len(currentDirectory)-2]))
+                        
+                        print(str(currentDirectory))
+                        if len(currentDirectory) == 1:
+                            MainDir.findParentAddChild("/", AdventDir(strLine.split(" ")[2], 0, []))
+                        else:
+                            MainDir.findParentAddChild(currentDirectory[len(currentDirectory)-2], AdventDir(strLine.split(" ")[2], 0, []))
+
+
+                        #allDirectories[strLine.split(" ")[2]] = ' '.join(currentDirectory)
                     else:
                         del currentDirectory[-1]
 
 
-            else:
-                if strLine.split(" ")[0] != "dir":
-                    if currentDirectory[len(currentDirectory)-1] not in documentReverseIndex:
-                        documentReverseIndex[currentDirectory[len(currentDirectory)-1]] = int(strLine.split(" ")[0])
-                    else:
-                        documentReverseIndex[currentDirectory[len(currentDirectory)-1]] += int(strLine.split(" ")[0])
+            #else:
+                #if strLine.split(" ")[0] != "dir":
+                    #if currentDirectory[len(currentDirectory)-1] not in documentReverseIndex:
+                    #    documentReverseIndex[currentDirectory[len(currentDirectory)-1]] = int(strLine.split(" ")[0])
+                    #else:
+                    #    documentReverseIndex[currentDirectory[len(currentDirectory)-1]] += int(strLine.split(" ")[0])
 
-    #print("The answer is: " + str(allDirectories))
+    print(MainDir.children)
 
-    totalSized = dict()
 
-    for key in allDirectories:
-        totalValue = 0
+    #totalSized = dict()
 
-        for strTest in str(allDirectories[key]).split(" "):
-            if (strTest != "/"):
-                if strTest in documentReverseIndex:
-                    print(str(allDirectories[key]) + " - " + strTest + " - " + str(documentReverseIndex[strTest]))
+    #for key in allDirectories:
+    #    totalValue = 0
+
+    #    for strTest in str(allDirectories[key]).split(" "):
+    #        if (strTest != "/"):
+    #            if strTest in documentReverseIndex:
+    #                print(str(allDirectories[key]) + " - " + strTest + " - " + str(documentReverseIndex[strTest]))
 
     #for key in allDirectories:
     #    if str(allDirectories[key]) in documentReverseIndex:
