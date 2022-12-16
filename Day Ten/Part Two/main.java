af918bfd7859963c6ec366dfe1137342e5000c04
J.Morin <jacobmorin61@yahoo.ca>
2022-12-15 20:31:05 -0500

    Finished day 10

--- Day Ten/Part Two/main.java
+++ Day Ten/Part Two/main.java
@@ -6,85 +6,24 @@ class CPU {
     HashMap<Integer, Integer> SignalHistory = new HashMap<>();
     int currentCycle = 0;
     int XValue = 1;
-    CRT objCRT = new CRT();
 
     public CPU() {
         SignalHistory.put(currentCycle, XValue);
     }
-
-    public void DrawScreen() {
-        for (Integer signal : this.SignalHistory.values()) {
-            this.objCRT.insertRow(signal);
-        }
-
-        this.objCRT.DrawScreen();
-    }
-
     public void addNoop() {
         currentCycle++;
         SignalHistory.put(currentCycle, XValue);
-        //objCRT.insertRow(this.SignalHistory.get(currentCycle));
     }
-
     public void addX(int X) {
-        for (int i = 1; i < 3; i++) {
+        for (int i = 0; i < 2; i++) {
             currentCycle++;
             SignalHistory.put(currentCycle, XValue);
-            //objCRT.insertRow(this.SignalHistory.get(currentCycle));
         }
 
         XValue += X;
     }
-
-    public int fetchFrame(int targetFrame) {
-        return targetFrame * this.SignalHistory.get(targetFrame);
-    }
-}
-
-class CRT {
-    //Start from left to right
-    //String[][] Screen = new String[6][40];
-
-    List<String> Screen = new ArrayList<>();
-
-    public void insertRow(int XValue) {
-        String value = ".";
-
-        if (XValue == (this.Screen.size()-1) || XValue+1 == (this.Screen.size()-1) || XValue+2 == (this.Screen.size()-1)) {
-            value = "#";
-        }
-
-        this.Screen.add(value);
-
-        /*for (int drawn = 0; drawn < XValue; drawn++) {
-            if (drawn == XValue - 1) {
-                System.out.print("### \n");
-
-                if (Screen.size() == XValue) {
-                    Screen.add("#");
-                } else {
-                    Screen.add(".");
-                }
-            } else {
-                System.out.print(".");
-            }
-        }*/
-    }
-
-    public void DrawScreen() {
-        int cycle = 1;
-        for (String pixel : this.Screen) {
-            //this.objCRT.insertRow(signal);
-            System.out.print(pixel);
-
-            if (cycle == 40 || cycle == 80 || cycle == 120 || cycle == 160 || cycle == 200 || cycle == 240) {
-                System.out.println();
-            }
-
-            cycle++;
-        }
-
-        System.out.println(this.Screen);
+    public int fetchAddX(int target) {
+        return this.SignalHistory.get(target);
     }
 }
 
@@ -102,13 +41,37 @@ class TenTwo {
             }
         }
 
-        objCPU.DrawScreen();
+        for (int keyVal : objCPU.SignalHistory.keySet()) {
+            boolean writePixel = false;
 
-        /*System.out.println(objCPU.SignalHistory);
-        System.out.println(objCPU.calculateSum());*/
+            if (keyVal > 0 && keyVal < 40) {
+                writePixel = (keyVal >= objCPU.fetchAddX(keyVal) && keyVal <= objCPU.fetchAddX(keyVal)+2);
+            } else if (keyVal > 39 && keyVal < 80) {
+                int diff = keyVal - 40;
+                writePixel = (diff >= objCPU.fetchAddX(keyVal) && diff <= objCPU.fetchAddX(keyVal)+2);
+            } else if (keyVal > 79 && keyVal < 120) {
+                int diff = keyVal - 80;
+                writePixel = (diff >= objCPU.fetchAddX(keyVal) && diff <= objCPU.fetchAddX(keyVal)+2);
+            } else if (keyVal > 119 && keyVal < 160) {
+                int diff = keyVal - 120;
+                writePixel = (diff >= objCPU.fetchAddX(keyVal) && diff <= objCPU.fetchAddX(keyVal)+2);
+            } else if (keyVal > 159 && keyVal < 200) {
+                int diff = keyVal - 160;
+                writePixel = (diff >= objCPU.fetchAddX(keyVal) && diff <= objCPU.fetchAddX(keyVal)+2);
+            } else {
+                int diff = keyVal - 200;
+                writePixel = (diff >= objCPU.fetchAddX(keyVal) && diff <= objCPU.fetchAddX(keyVal)+2);
+            }
 
+            if (writePixel) {
+                System.out.print("#");
+            } else {
+                System.out.print(" ");
+            }
 
-        //objCRT.DrawScreen();
-        //objCRT.insertRow(objCPU.SignalHistory.get(1));
+            if (keyVal == 39 || keyVal == 79 || keyVal == 119 || keyVal == 159 || keyVal == 199 ) {
+                System.out.println();
+            }
+        }
     }
-} 
+}
